2f4c4c25dff18c6d93c3c7c2ffacc58ea2b9356d
Sebastian <asamk@gmx.de>
2016-03-06 14:04:03 +0100

    Complete rebuild when making nuget package

--- make_package.bat
+++ make_package.bat
@@ -2,7 +2,7 @@ REM Build Solution
 set PATH_SOURCE_PROJ="%cd%\TranslationLib.vbproj"
 
 FOR %%C IN (Release20 Release40) DO (
- "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" %PATH_SOURCE_PROJ% /p:Configuration=%%C
+ "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" %PATH_SOURCE_PROJ% /t:Rebuild /p:Configuration=%%C
  if errorlevel 1 goto :eof
 )
 
